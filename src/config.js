2a6c544b350da617bfed250ff87c1368a20da664
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-10-18 17:11:39 -0600

    Updates

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const emailJS_User_ID = "2LyUuuoQJRUC7DB1x";
-export const emailJS_Access_Token = "BOLenH6QTkc3Deq0mTo_y";
-export const emailJS_template_id = "template_r9595mj";
-export const emailJS_service_id = "ahmads_gmail";
